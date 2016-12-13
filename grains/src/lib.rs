eba962a0b27572c194c49514e43091148677f9ec
Eric Petersen <eric@huskers.unl.edu>
2016-12-12 18:23:13 -0800

    Parameterize min and max

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,12 +1,14 @@
 const TWO: u64 = 2;
+const MIN: u32 = 1;
+const MAX: u32 = 64;
 
 pub fn square(s: u32) -> u64 {
-    if s < 1 || s > 64 {
+    if s < MIN || s > MAX {
         panic!("Square must be between 1 and 64");
     }
     TWO.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    (1..65).fold(0, |acc, s| acc + square(s))
+    (MIN..MAX + 1).fold(0, |acc, s| acc + square(s))
 }
